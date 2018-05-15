b86dbf842a940cef541ac796364765f538e3c6af
harshmmodi <harshmmodi@hotmail.com>
2018-05-15 23:40:44 +0530

    comment added

--- app/src/main/java/com/example/harshmodi/calculator/MainActivity.java
+++ app/src/main/java/com/example/harshmodi/calculator/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // agjkbknlkmlkk
 
         backspace = (Button)findViewById(R.id.buttonBack);
         clr = (Button)findViewById(R.id.buttonC);
